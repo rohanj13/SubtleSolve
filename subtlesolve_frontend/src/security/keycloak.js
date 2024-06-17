1728d9f0237d51fb7044530597eb41bc13c9e28b
rohanj13 <rohan.australia@gmail.com>
2024-06-18 00:53:13 +1000

    trying ip

--- subtlesolve_frontend/src/security/keycloak.js
+++ subtlesolve_frontend/src/security/keycloak.js
@@ -1,6 +1,6 @@
 import Keycloak from 'keycloak-js'
 const keycloakConfig = {
-  url: 'http://keycloak:8080', 
+  url: 'http://140.238.195.136:8080', 
   realm: 'SubtleSolveAuth', 
   clientId: 'subtle-solve-auth'
 }
