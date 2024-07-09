5601361cf1f1961419dca74e22fce2423b117578
rohanj13 <rohan.australia@gmail.com>
2024-07-09 19:16:46 +1000

    updated admin url

--- subtlesolve_frontend/src/security/keycloak.js
+++ subtlesolve_frontend/src/security/keycloak.js
@@ -1,6 +1,6 @@
 import Keycloak from 'keycloak-js'
 const keycloakConfig = {
-  url: 'https://auth.subtlesolve.org', 
+  url: 'https://www.subtlesolve.org', 
   realm: 'SubtleSolveAuth', 
   clientId: 'subtle-solve-auth'
 }
